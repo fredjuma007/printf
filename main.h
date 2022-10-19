115294994eff4b9bd33dd07327fa2408a3c0731a
fredjuma007 <fredjuma8@gmail.com>
2022-10-19 14:58:19 -0700

    leap of faith

--- main.h
+++ main.h
@@ -9,11 +9,18 @@
 char* (*get_func(char i))(va_list);
 char *create_buffer(void);
 void write_buffer(char *buffer, int len, va_list list);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 /* printf functions */
 int _printf(const char *format, ...);
 char *print_s(va_list list);
 char *print_c(va_list list);
+char *print_d(va_list list);
+char *itob(va_list list);
+char *rot13(va_list list);
+char *rev_string(va_list list);
+char *itoOctal(va_list list);
 
 /**
  * struct types - struct
