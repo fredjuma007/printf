c02a61a199a8f7fad17421baca8c4c06ac8f82ab
fredjuma007 <fredjuma8@gmail.com>
2022-10-18 17:40:44 -0700

    it runs in my machine

--- main.h
+++ main.h
@@ -4,17 +4,28 @@
 #include <stdarg.h> /*va_list is fount here*/
 #include <stdlib.h> /*mallo, free*/
 #include <unistd.h> /* write the buffer*/
-
-/*printf functions*/
-int _printf(const char *format, ...); /*the actual printf*/
-char *print_s(va_list list);
-char *print_c(va_list list);
-char *print_d(va_list list);
-char *itob(va_list list);
-char *rot13(va_list list);
-char *rev_string(va_list list); /*reverses the string*/
-
-/*assist functions, mostly custom*/
+#include <string.h>
+/* assisting functions */
 char* (*get_func(char i))(va_list);
 char *create_buffer(void);
+void write_buffer(char *buffer, int len, va_list list);
+
+/* printf functions */
+int _printf(const char *format, ...);
+char *print_s(va_list list);
+char *print_c(va_list list);
+
+/**
+ * struct types - struct
+ * @id: identifier of type to print (e.g. c means char)
+ * @func: ptr to functions that print according to identifier (e.g. print_c)
+ */
+
+typedef struct types
+{
+	char id;
+	char* (*func)(va_list);
+} print;
+
+
 #endif
