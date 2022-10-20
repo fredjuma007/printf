9834af99bfab4e31ef8642886ee3aa55d95d9d37
fredjuma007 <fredjuma8@gmail.com>
2022-10-19 17:41:00 -0700

    coments

--- driver.c
+++ driver.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * driver - selector for type of fun.
+ * driver - selector for type of function.
  * @format: string.
  * Description: the function loops through the structs
  * selector[] ment of the struct.
@@ -12,19 +12,22 @@
 int (*driver(char *format))(char *format, va_list)
 {
 int i;
-
+/*So Chelsea this besicaly are all the fuctions
+ * the project is to handle and evry file
+ * will select its function
+ * from this file */
 structype selector[] = {
-{"%c", printc},
-{"%s", printstr},
-{"%d", printint},
+{"%c", printc}, /*c */
+{"%s", printstr},/*string */
+{"%d", printint},/*integer */
 {"%i", printint},
-{"%%", printpercent},
-{"%x", printhex},
+{"%%", printpercent},/*this % thing*/
+{"%x", printhex},/*hexadecimal*/
 {"%X", printHEX},
-{"%o", printocta},
+{"%o", printocta},/*octal*/
 {NULL, NULL}
 };
-
+/*nullifying the return*/
 if (format[1] == ' ' || format[1] == '\0')
 return (NULL);
 for (i = 0; selector[i].q; i++)
